2fa34c059672d50071d2c0682db672f8b868551b
son <son.nguyen@gleads.vn>
2025-11-18 17:29:18 +0700

    add theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AppSidebar from "@/components/AppSidebar";
 import Navbar from "@/components/Navbar";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,11 +30,18 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex`}
       >
-        <AppSidebar />
-        <main className="w-full">
-          <Navbar />
-          <div className="px-4">{children}</div>
-        </main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <AppSidebar />
+          <main className="w-full">
+            <Navbar />
+            <div className="px-4">{children}</div>
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-import { LogOut, Moon, Settings, User } from "lucide-react";
+import { LogOut, Moon, Settings, SquareMenu, User } from "lucide-react";
 import Link from "next/link";
 import {
   DropdownMenu,
@@ -9,6 +9,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
@@ -31,11 +32,43 @@ const Navbar = () => {
             <DropdownMenuContent sideOffset={9}>
               <DropdownMenuLabel>My Account</DropdownMenuLabel>
               <DropdownMenuSeparator />
-              <DropdownMenuItem className="flex items-center gap-2"> <User className="h-[1.2rem] w-[1.2rem] mr-2"/> Profile</DropdownMenuItem>
-              <DropdownMenuItem className="flex items-center gap-2"><Settings className="h-[1.2rem] w-[1.2rem] mr-2"/> Setting</DropdownMenuItem>
-              <DropdownMenuItem className="flex items-center gap-2" variant="destructive"><LogOut className="h-[1.2rem] w-[1.2rem] mr-2"/> Logout</DropdownMenuItem>
+              <DropdownMenuItem className="flex items-center gap-2">
+                <User className="h-[1.2rem] w-[1.2rem] mr-2" /> Profile
+              </DropdownMenuItem>
+              <DropdownMenuItem className="flex items-center gap-2">
+                <Settings className="h-[1.2rem] w-[1.2rem] mr-2" /> Setting
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="flex items-center gap-2"
+                variant="destructive"
+              >
+                <LogOut className="h-[1.2rem] w-[1.2rem] mr-2" /> Logout
+              </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenuTrigger>
+        </DropdownMenu>
+        <DropdownMenu>
+          {/* button not inside button use asChild help it, display child not default element*/}
+          <DropdownMenuTrigger asChild>
+            <Button variant={"outline"} size={"icon"}>
+              <SquareMenu />
+              <span className="sr-only">Open Menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent sideOffset={9}>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="flex items-center gap-2">
+              Menu Item 1
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex items-center gap-2">
+              Menu Item 2
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex items-center gap-2">
+              Menu Item 3
+            </DropdownMenuItem>
+          </DropdownMenuContent>
         </DropdownMenu>
       </div>
     </nav>
