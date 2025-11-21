6f641d450ca11095134981ea0e944c8065fe5697
son <son.nguyen@gleads.vn>
2025-11-21 17:52:43 +0700

    create chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
+import AppAreAChart from "@/components/AppAreaChart";
+import AppBarChart from "@/components/AppBarChart";
+import AppPieChart from "@/components/AppPieChart";
+import CardList from "@/components/CardList";
+
 export default function Home() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
-      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-        ab
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-2 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-1 xl:col-span-1 2xl:col-span-1">
+        <CardList title="Popular Content" />
+      </div>
+
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-1 xl:col-span-1 2xl:col-span-1">
+        <AppPieChart />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg"></div>
-      <div className="bg-primary-foreground p-4 rounded-lg"></div>
-      <div className="bg-primary-foreground p-4 rounded-lg"></div>
-      <div className="bg-primary-foreground p-4 rounded-lg"></div>
-      <div className="bg-primary-foreground p-4 rounded-lg"></div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-2 2xl:col-span-2">
+        <AppAreAChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <CardList title="Popular Content" />
+      </div>
       <div className="bg-primary-foreground p-4 rounded-lg"></div>
     </div>
   );
--- components/AppBarChart.tsx
+++ components/AppBarChart.tsx
@@ -1,3 +1,14 @@
+"use client";
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "./ui/chart";
+
 const AppBarChart = () => {
   const chartData = [
     { month: "January", desktop: 186, mobile: 80 },
@@ -7,7 +18,39 @@ const AppBarChart = () => {
     { month: "May", desktop: 209, mobile: 130 },
     { month: "June", desktop: 214, mobile: 140 },
   ];
-  return <div>AppBarChart</div>;
+
+  const chartConfig = {
+    desktop: {
+      label: "Desktop",
+      color: "var(--chart-3)",
+    },
+    mobile: {
+      label: "Mobile",
+      color: "var(--chart-2)",
+    },
+  } satisfies ChartConfig;
+  return (
+    <div className="">
+      <h1 className="text-lg font-medium mb-6">Total Revenue</h1>
+      <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+        <BarChart accessibilityLayer data={chartData}>
+          <CartesianGrid vertical={false} />
+          <XAxis
+            dataKey="month"
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+            tickFormatter={(value) => value.slice(0, 3)}
+          />
+          <YAxis tickLine={false} tickMargin={10} axisLine={false} />
+          <ChartTooltip content={<ChartTooltipContent />} />
+          <ChartLegend content={<ChartLegendContent />} />
+          <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+          <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+        </BarChart>
+      </ChartContainer>
+    </div>
+  );
 };
 
 export default AppBarChart;
